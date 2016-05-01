c7d9ea8231fc39e0970c117cd2b05b247996fe22
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-04-30 22:38:25 -0400

    Fixing setup.py version, name.

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #! /usr/bin/env python
 
 from distutils.core import setup
-setup(name="vy",
-      version="0.1",
+setup(name="steinitz",
+      version="1.0.0",
       packages=["steinitz"],
       scripts=['snz'],
       package_data={'steinitz': ['icon/*.gif', 'style/*/*.gif']},
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br")
+
 
 
 
